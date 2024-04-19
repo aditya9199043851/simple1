027e34b4f78eb7779300451dff183f06efb13871
aditya singh <singhadarsh1611@gmail.com>
2024-04-19 10:11:53 +0530

    Initial Commit

---
+++ simple/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  simple
+//
+//  Created by Apple on 19/04/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
